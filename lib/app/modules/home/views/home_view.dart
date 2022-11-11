1c2b4a8d39f48864ea960cd22402dd91997530f0
WidyaM <Wdyamegas@gmail.com>
2022-11-11 22:29:53 +0700

    Pertemuan3

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:task_management_app/app/utils/widget/header.dart';
+import 'package:task_management_app/app/utils/widget/sideBar.dart';
 
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+
   const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      backgroundColor: Colors.blue[100],
+      body: Row(
+        children: [
+          const Expanded(
+            flex: 2,
+            child: SideBar(),
+          ),
+          Expanded(
+          flex: 15,
+          child: Column(children: [
+            const header(),
+            Expanded(
+              child: Container(
+              padding: const EdgeInsets.all(50),
+              margin: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),),
+              ),
+            ), 
+          ]),
+          )
+        ],
       ),
     );
   }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,14 +5,16 @@ abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
   static const PROFILE = _Paths.PROFILE;
-  static const ABOUT = _Paths.ABOUT;
   static const FRIENDS = _Paths.FRIENDS;
+  static const LOGIN = _Paths.LOGIN;
+  static const TASK = _Paths.TASK;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
   static const PROFILE = '/profile';
-  static const ABOUT = '/about';
   static const FRIENDS = '/friends';
+  static const LOGIN = '/login';
+  static const TASK = '/task';
 }
