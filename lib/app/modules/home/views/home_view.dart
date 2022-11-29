3e8493ddb9d72621f6da3a796632abc2e2e662ea
WidyaM <Wdyamegas@gmail.com>
2022-11-29 13:25:29 +0700

    commit3

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,41 +1,94 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-import 'package:task_management_app/app/utils/widget/header.dart';
-import 'package:task_management_app/app/utils/widget/sideBar.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:task_management_app/app/routes/app_pages.dart';
 
-import '../controllers/home_controller.dart';
+class header extends StatelessWidget {
+  const header({
+    Key? key,
+  }) : super(key: key);
 
-class HomeView extends GetView<HomeController> {
-
-  const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue[100],
-      body: Row(
-        children: [
-          const Expanded(
-            flex: 2,
-            child: SideBar(),
+    return SizedBox(
+      height: Get.height * 0.1, 
+      child: Padding(
+        padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          Column(
+            //mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+                Text(
+                'Task Management', style: TextStyle(fontSize: 22, color: Colors.grey,),
+                ),
+                Text(
+                'Manage Task made easy with friends', 
+                style: TextStyle(fontSize: 15 , color: Colors.grey,),),
+            ],
           ),
-          Expanded(
-          flex: 15,
-          child: Column(children: [
-            const header(),
-            Expanded(
-              child: Container(
-              padding: const EdgeInsets.all(50),
-              margin: const EdgeInsets.all(10),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(50),),
+        
+    const Spacer(flex: 1), 
+    Expanded(                
+        flex: 1,              
+        child: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            contentPadding: 
+              const EdgeInsets.only(left: 40, right: 10),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30), 
+              borderSide: const BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: Colors.blue),
+            ),
+            prefixIcon: const Icon(
+              Icons.search, 
+              color: Colors.grey,
+            ),
+            hintText: 'Search',
+          ),
+          ),
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        const Icon(Ionicons.notifications, color: Colors.grey,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        GestureDetector(
+          onTap: (){
+            Get.defaultDialog(
+              title: 'Sign Out',
+              content: const Text('Are you Sure to sign out'),
+              cancel: ElevatedButton(onPressed: () => Get.back(), child: const Text('Cancle'),),
+              confirm: ElevatedButton(onPressed: () => Get.toNamed(Routes.LOGIN), child: const Text('Sign Out'),),
+              );
+          },
+          child: Row(
+            children: const [
+              Text('Sign Out', 
+              style: TextStyle(color: Colors.grey),
               ),
-            ), 
-          ]),
-          )
-        ],
-      ),
-    );
+              SizedBox(
+            width: 5,
+            ),
+            Icon(Ionicons.log_out_outline, 
+            color: Colors.grey,
+            ),
+            ],                
+          ),
+        ),
+
+      ]),
+    ),
+          );
   }
 }
