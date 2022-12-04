2a4de57e90fb4a22586bdce5fbb84e662c21086f
WidyaM <Wdyamegas@gmail.com>
2022-12-04 22:08:38 +0700

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_pertama/app/controller/auth_controller.dart';
+import 'package:flutter_pertama/firebase_options.dart';
 
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
-  runApp(
-    GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  Get.put(AuthController(), permanent: true);
+  runApp(StreamBuilder<User?>(
+    stream: FirebaseAuth.instance.authStateChanges(),
+    builder: (context, snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(child: CircularProgressIndicator());
+      }
+      return GetMaterialApp(
+      debugShowCheckedModeBanner: false, 
+      title: "Application",
+      initialRoute: snapshot.data !=null? Routes.HOME : Routes.LOGIN,
+      getPages: AppPages.routes, 
+    );
+  },
+));
 }
